396514fc2c6f66c6df214426292ac8878d98ad81
cemalkilickaya <43484779+cemalkilickaya@users.noreply.github.com>
2024-12-06 04:04:18 +0300

    cors permission fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,10 @@ useContainer(Container);
 export const app = createExpressServer({
   controllers: [BuildingController],
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://water-consumption-dashboard-1r7d70tpi-cemals-projects-1d23365a.vercel.app/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   },
